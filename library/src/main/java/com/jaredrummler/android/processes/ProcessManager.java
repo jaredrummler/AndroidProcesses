e0a1ac7917c58adf72e1bcb4619b67c0d75a4c3e
Jared Rummler <jared@jaredrummler.com>
2015-11-11 16:24:21 -0800

    Add class to filter processes by name/pid.

--- library/src/main/java/com/jaredrummler/android/processes/ProcessManager.java
+++ library/src/main/java/com/jaredrummler/android/processes/ProcessManager.java
@@ -100,7 +100,8 @@ public class ProcessManager {
   /**
    * Get a list of user apps running in the foreground.
    *
-   * @param ctx the application context
+   * @param ctx
+   *     the application context
    * @return a list of user apps that are in the foreground.
    */
   public static List<AndroidAppProcess> getRunningForegroundApps(Context ctx) {
@@ -145,7 +146,6 @@ public class ProcessManager {
   }
 
   /**
-   *
    * @return {@code true} if this process is in the foreground.
    */
   public static boolean isMyProcessInTheForeground() {
@@ -182,6 +182,109 @@ public class ProcessManager {
     }
     ActivityManager am = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
     return am.getRunningAppProcesses();
+  }
+
+  /**
+   * Get a list of running processes based on name, pid, ppid, or other conditions.
+   *
+   * <p>Example usage:</p>
+   *
+   * <pre>
+   *   // Get all processes that contain the name "google"
+   *   Filter filter = new ProcessManager.Filter().setName("google");
+   *   List&lt;AndroidProcess&gt; processes = filter.run();
+   * </pre>
+   */
+  public static class Filter {
+
+    private String name;
+    private int pid = -1;
+    private int ppid = -1;
+    private boolean apps;
+
+    /**
+     * @param name
+     *     The name of the process to filter
+     * @return This Filter object to allow for chaining of calls to set methods
+     */
+    public Filter setName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    /**
+     * @param pid
+     *     The process id to filter
+     * @return This Filter object to allow for chaining of calls to set methods
+     */
+    public Filter setPid(int pid) {
+      this.pid = pid;
+      return this;
+    }
+
+    /**
+     * @param ppid
+     *     The parent process id to filter
+     * @return This Filter object to allow for chaining of calls to set methods
+     */
+    public Filter setPpid(int ppid) {
+      this.ppid = ppid;
+      return this;
+    }
+
+    /**
+     * @param apps
+     *     {@code true} to only filter app processes
+     * @return This Filter object to allow for chaining of calls to set methods
+     */
+    public Filter setApps(boolean apps) {
+      this.apps = apps;
+      return this;
+    }
+
+    /**
+     * @return a List of processes based on the filter options.
+     */
+    public List<AndroidProcess> run() {
+      List<AndroidProcess> processes = new ArrayList<>();
+      File[] files = new File("/proc").listFiles();
+      for (File file : files) {
+        if (file.isDirectory()) {
+          int pid;
+          try {
+            pid = Integer.parseInt(file.getName());
+          } catch (NumberFormatException e) {
+            continue;
+          }
+          if (this.pid != -1 && pid != this.pid) {
+            continue;
+          }
+          try {
+            AndroidProcess process;
+            if (this.apps) {
+              process = new AndroidAppProcess(pid);
+            } else {
+              process = new AndroidProcess(pid);
+            }
+            if (this.name != null && !process.name.contains(this.name)) {
+              continue;
+            }
+            if (this.ppid != -1 && process.stat().ppid() != this.ppid) {
+              continue;
+            }
+            processes.add(process);
+          } catch (IOException e) {
+            // If you are running this from a third-party app, then system apps will not be
+            // readable on Android 5.0+ if SELinux is enforcing. You will need root access or an
+            // elevated SELinux context to read all files under /proc.
+            // See: https://su.chainfire.eu/#selinux
+          } catch (AndroidAppProcess.NotAndroidAppProcessException ignored) {
+          }
+        }
+      }
+      return processes;
+    }
+
   }
 
   /**
